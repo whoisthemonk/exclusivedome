6a6e5e5e2fe56b1c728f7eb52d4859f6814800b9
whoisthemonk <whoisthemonk1@gmail.com>
2025-04-09 23:39:51 +0100

    lodgify-proxy.js

--- netlify/functions/lodgify-proxy.js
+++ netlify/functions/lodgify-proxy.js
@@ -1,10 +1,16 @@
-
 export async function handler(event) {
   const { from, to, propertyId } = event.queryStringParameters;
 
+  if (!from || !to || !propertyId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing required query parameters" }),
+    };
+  }
+
   const response = await fetch(`https://public-api.lodgify.com/v2/calendar/availability?propertyId=${propertyId}&from=${from}&to=${to}`, {
     headers: {
-      'Authorization': 'Bearer YOUR_API_TOKEN_HERE'
+      'Authorization': 'Bearer oZrpljNpDIln9Im8drC1zjFtGifpOKsgQSi+AAS3isFyU1aNoWG520kQX/R+iaXm'
     }
   });
 
@@ -15,7 +21,9 @@ export async function handler(event) {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
     },
     body: JSON.stringify(data)
   };
 }
+
